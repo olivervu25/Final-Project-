2bd632407274def353dc1d4c1089854705c9c1c9
longvt <longvu2000.hust@gmail.com>
2020-07-24 16:20:47 +0700

    Login Screen

--- js/index.js
+++ js/index.js
@@ -2,4 +2,20 @@ const init = () => {
     console.log('Window loaded')
     view.setActiveScreen('registerScreen')
 }
-window.onload = init 
+window.onload = init
+
+// document.addEventListener('click',function goToRegister () {
+//     view.setActiveScreen('registerScreen');
+// },
+//     document.addEventListener('click',function goToLogin() {
+//         view.setActiveScreen('loginScreen');
+//     })
+//)
+function goToRegister() {
+    document.getElementById('redirect-to-register').style.color = "red";
+    view.setActiveScreen('registerScreen')
+}
+function goToLogin() {
+    document.getElementById('redirect-to-login').style.color = "red";
+    view.setActiveScreen('loginScreen')
+}
